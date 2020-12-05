4849463aae385fc5643d54745f0113698e054e72
Sean Beaudoin <s10_n@mailbox.org>
2020-12-05 10:04:33 -0800

    finished day 5

---
+++ 5/5.1.py
@@ -0,0 +1,35 @@
+boarding_passes_file = open('input','r')
+boarding_passes = []
+
+# write each line in the input file into a list of boarding passes
+for line in boarding_passes_file:
+    boarding_passes.append(line[:-1])
+
+seat_ids = [] # create an empty list of seat IDs
+
+for boarding_pass in boarding_passes:
+    # reset possible seats
+    row_range,column_range = [],[]
+    for row in range(128): row_range.append(row) # create 128 possible rows
+    for column in range(8): column_range.append(row) # create 8 possible columns
+   
+    for letter in boarding_pass[:-3]: # use the first seven letters to determine the row
+        rows = int(len(row_range) / 2) 
+        if letter == 'F':
+            row_range = row_range[:rows] # look in the first half
+        elif letter == 'B':
+            row_range = row_range[rows:] # look in the second half
+        row = row_range[0]
+
+    for letter in boarding_pass[-3:]: # use the last three letters to determine the column
+        columns = int(len(column_range) / 2)
+        if letter == 'L':
+            column_range = column_range[:columns] # look in the first half
+        elif letter == 'R':
+            column_range = column_range[columns:] # look in the second half
+        column = column_range[0]
+
+    seat_ids.append(row * 8 + column) # add the seat ID to the list
+
+seat_ids.sort(reverse=True)
+print(seat_ids[0])
